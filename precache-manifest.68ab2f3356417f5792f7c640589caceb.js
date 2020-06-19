f38b8c54f008b862a87e7d188f39e843ec87ba7b
Daniel Marks <danielpresliermarks@gmail.com>
2020-06-19 03:00:17 -0400

    Updates

---
+++ precache-manifest.68ab2f3356417f5792f7c640589caceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ba7cb4f516f73c2c24e5d7eb300cac",
+    "url": "/vinyl-stream/index.html"
+  },
+  {
+    "revision": "19aadad7076e1f9a7b3a",
+    "url": "/vinyl-stream/static/css/main.d13890b5.chunk.css"
+  },
+  {
+    "revision": "aceab9145d06bc13b4a5",
+    "url": "/vinyl-stream/static/js/2.3d126011.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vinyl-stream/static/js/2.3d126011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19aadad7076e1f9a7b3a",
+    "url": "/vinyl-stream/static/js/main.819db3a7.chunk.js"
+  },
+  {
+    "revision": "01a9212466ff989420db",
+    "url": "/vinyl-stream/static/js/runtime-main.5cc6e8f2.js"
+  },
+  {
+    "revision": "abbff554dc5e877b9d9b8d23366d6b63",
+    "url": "/vinyl-stream/static/media/Spotify-Logo.abbff554.png"
+  }
+]);
